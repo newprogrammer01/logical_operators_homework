6dc827ffacf76f44807f501e6b4f8ae0311c0b4e
newprogrammer01 <jasurbekpirnazarov09@gmail.com>
2022-10-20 15:42:35 +0500

    1313

--- logic13.py
+++ logic13.py
@@ -6,9 +6,6 @@ def main(a):
     Returns:
         bool: answer
     """
-    if (a<=-10 and a>-100 and a%2==0)  or (a>=10 and 100>a  and a%2==0):
-     return("True")
-    else:
-     return("False")
+    return((a<=-10 and a>-100 and a%2==0)  or (a>=10 and 100>a  and a%2==0))
 
-print(main(-46))
+print(main(25))
