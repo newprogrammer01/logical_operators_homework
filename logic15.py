c29396778d86658e55eac17daf75bfcc0fd5a6a4
newprogrammer01 <jasurbekpirnazarov09@gmail.com>
2022-10-20 17:08:22 +0500

    15

--- logic15.py
+++ logic15.py
@@ -6,4 +6,9 @@ def main(a):
     Returns:
         bool: answer
     """
-    return
+    x1=a//100
+    x2=a%100//10
+    x3=a%10
+    x4=x1+x2+x3
+    return (a>=100 and a<1000 and (x4)%2==1)
+print(main(264))
