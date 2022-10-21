91f0d7b4f925680abf870817b383110eede97945
newprogrammer01 <jasurbekpirnazarov09@gmail.com>
2022-10-21 09:58:19 +0500

    1717

--- logic17.py
+++ logic17.py
@@ -6,4 +6,20 @@ def main(a):
     Returns:
         bool: answer
     """
-    return
+    #a=12345
+    x1=a%10
+    a=a//10
+
+    x2=a%10
+    a=a//10
+
+    x3=a%10
+    a=a//10
+
+    x4=a%10
+    x5=a//10
+    return (x1<=x2 and x2<=x3 and x3<=x4 and x4<=x5)
+print(main(12345))
+\
+
+  
